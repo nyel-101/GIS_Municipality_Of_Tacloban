f9c6f380b7b0014c33e165189813618030406d26
nyel-101 <dandevjr@gmail.com>
2025-02-14 18:57:59 +0000

    style(config): restructure view templates

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6cb10b96e30c4f789aa683f61580b294 = () => {
-  return '2026-05-28T18:22:11Z';
+const node_557ae028591847aab05e0e548fe594d8 = () => {
+  return '2025-02-14T18:57:59Z';
 };
