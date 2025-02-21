d173afcb3bcd14e43a10f24e2cfe7a47ed9ce787
nyel-101 <dandevjr@gmail.com>
2025-02-21 01:05:42 +0000

    chore(core): enhance database index

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1cbda37aaf0745b1b53ab8784795f297 = () => {
-  return '2026-03-21T09:40:33Z';
+const node_6ed4fe1eefef4ac1969faa1b692650f9 = () => {
+  return '2025-02-21T01:05:42Z';
 };
