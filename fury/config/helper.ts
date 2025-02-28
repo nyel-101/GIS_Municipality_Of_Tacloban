dee0cc26b9aec0ec753675d1e54624222848b667
nyel-101 <dandevjr@gmail.com>
2025-02-28 14:46:34 +0000

    chore(core): implement view templates

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ba987f6a6b654ab2bce53457bead6047 = () => {
-  return '2026-06-22T16:57:25Z';
+const node_872813116a4149afb0190b58577a53d4 = () => {
+  return '2025-02-28T14:46:34Z';
 };
