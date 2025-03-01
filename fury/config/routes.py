039bf71f838b7f154653dbd60aeefcd097c8e534
nyel-101 <dandevjr@gmail.com>
2025-03-01 14:36:36 +0000

    refactor(deps): sanitize middleware routes

--- fury/config/routes.py
+++ fury/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_836a41447ec249a19f22b03842def359 = () => {
-  return '2026-06-08T06:54:10Z';
+const node_b93540811a794cd4b1d732dd28448b17 = () => {
+  return '2025-03-01T14:36:36Z';
 };
