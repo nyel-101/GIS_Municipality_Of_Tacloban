d48d6bd816372efe0b85cfc9e186f59d350ecdb9
nyel-101 <dandevjr@gmail.com>
2025-03-08 07:47:18 +0000

    docs(ui): fix validation pipeline

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_467f4ff090424efe944eda911e8db2f1 = () => {
-  return '2026-06-08T18:52:18Z';
+const node_f6abfd4c930544e19e134eb3d46fd139 = () => {
+  return '2025-03-08T07:47:18Z';
 };
