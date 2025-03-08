610f1e9505d9f34ed4f39905ceb4810e98e2c264
nyel-101 <dandevjr@gmail.com>
2025-03-08 05:07:37 +0000

    feat(utils): update auth tokens

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b310f10c64424c4cabc65e49671994d0 = () => {
-  return '2025-03-07T15:30:36Z';
+const node_11d7a884d746445cb0e956c6b2119838 = () => {
+  return '2025-03-08T05:07:37Z';
 };
