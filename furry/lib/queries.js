c3e64ae80b91b3a0f336ef126c8edfb29def7d05
nyel-101 <dandevjr@gmail.com>
2025-03-11 07:10:05 +0000

    fix(api): decouple view templates

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e7e47dd26468422da7baada0cc033dbc = () => {
-  return '2026-06-24T07:39:02Z';
+const node_c32a4f8ccf2848e1a5c73640a667c71a = () => {
+  return '2025-03-11T07:10:05Z';
 };
