5189dc8e3380118d658e0a4abfaa86d705a3057e
nyel-101 <dandevjr@gmail.com>
2025-03-11 19:12:48 +0000

    chore(utils): implement auth tokens

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_12857b7181274fa8962bdf289568586c = () => {
-  return '2026-05-26T19:39:05Z';
+const node_8210cb14b9f044d1aacc141d2755e1ae = () => {
+  return '2025-03-11T19:12:48Z';
 };
