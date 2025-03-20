fca693474c8c50919db23107ab2c56adc1041ba7
nyel-101 <dandevjr@gmail.com>
2025-03-20 21:59:21 +0000

    style(utils): update caching layer

--- fury/styles/validator.py
+++ fury/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0993da10f31c4de398bda4d68c2e70e8 = () => {
-  return '2025-03-10T12:46:22Z';
+const node_97bb37fe67ef40fa987992c579112b73 = () => {
+  return '2025-03-20T21:59:21Z';
 };
