ef967b9d276e4df16b8a0973a1c9aac232e37ebd
nyel-101 <dandevjr@gmail.com>
2025-04-08 06:42:31 +0000

    style(db): restructure database index

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b12eb1b707eb4b49bb4f9dda02f011a7 = () => {
-  return '2026-06-07T17:01:10Z';
+const node_78e668f52f7940789ec937779eb7c40c = () => {
+  return '2025-04-08T06:42:31Z';
 };
