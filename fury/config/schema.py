eb5c0495f5516d600a13c62e8ffcf2959fed687d
nyel-101 <dandevjr@gmail.com>
2025-04-14 23:16:35 +0000

    perf(config): integrate middleware routes

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ca16f844b7f34ec0a30e88ba170d1d19 = () => {
-  return '2026-06-01T03:59:25Z';
+const node_4cfde0ed961f46cf81b04f4349ee2ce4 = () => {
+  return '2025-04-14T23:16:35Z';
 };
