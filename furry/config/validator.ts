759e44ba3f05419d43aad9d06b08f3e57d93d778
nyel-101 <dandevjr@gmail.com>
2025-04-16 06:36:15 +0000

    chore(db): decouple auth tokens

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_769e138b711a466aad709b8e3d48b89c = () => {
-  return '2026-01-19T07:23:44Z';
+const node_f898ad4647fc413fb97d3533156d1c1d = () => {
+  return '2025-04-16T06:36:15Z';
 };
