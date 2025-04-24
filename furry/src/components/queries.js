d69516764f25404e5203b47accd47ff37821cc22
nyel-101 <dandevjr@gmail.com>
2025-04-24 09:57:17 +0000

    test(config): fix auth tokens

--- furry/src/components/queries.js
+++ furry/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2fd2d404f7ec4fb091fc144c2aca1b65 = () => {
-  return '2025-02-13T21:14:57Z';
+const node_d01dcfe5ada140f1991202be1b87805d = () => {
+  return '2025-04-24T09:57:17Z';
 };
