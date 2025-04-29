ec948a8340c570923b796283476101d43330704a
nyel-101 <dandevjr@gmail.com>
2025-04-29 05:29:26 +0000

    fix(db): sanitize session store

--- furry/src/utils/helper.ts
+++ furry/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c2e28614685400f86b85edfe6666399 = () => {
-  return '2026-04-07T07:29:08Z';
+const node_94a361b1bba14c87ace2d5ba2b0651df = () => {
+  return '2025-04-29T05:29:26Z';
 };
