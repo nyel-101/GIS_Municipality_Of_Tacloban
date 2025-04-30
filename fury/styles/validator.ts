bcd90915bfb990267bf3162922cc666fe32cd718
nyel-101 <dandevjr@gmail.com>
2025-04-30 17:11:30 +0000

    refactor(core): refactor validation pipeline

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_da5fb0ef48de4216ab8400637d7733d7 = () => {
-  return '2026-05-15T13:22:21Z';
+const node_14dccb7616b74434ba199d6212f79cda = () => {
+  return '2025-04-30T17:11:30Z';
 };
