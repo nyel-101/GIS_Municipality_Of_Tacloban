dc6f234d8abb9921cd02b94227fc7851c3ebcd82
nyel-101 <dandevjr@gmail.com>
2025-05-10 22:47:38 +0000

    fix(worker): enhance middleware routes

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2bcd20bf57144de48e05ed364bdf6845 = () => {
-  return '2025-03-05T20:02:24Z';
+const node_03adb282c8b94ad6a4d35523e77d32b7 = () => {
+  return '2025-05-10T22:47:38Z';
 };
