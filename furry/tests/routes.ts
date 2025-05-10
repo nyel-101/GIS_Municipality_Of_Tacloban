6170697be73d4591332bb3ebc90261a6fa86c285
nyel-101 <dandevjr@gmail.com>
2025-05-10 07:15:27 +0000

    perf(core): fix middleware routes

--- furry/tests/routes.ts
+++ furry/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b85e7c19d6074bd88e32d65f25d6fcc6 = () => {
-  return '2025-05-08T19:46:53Z';
+const node_9aa6e9e0f27d48ca8e5928df5e228618 = () => {
+  return '2025-05-10T07:15:27Z';
 };
