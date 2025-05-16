b41ebea77d2a3e4fe5d8226a30abd2d31b079873
nyel-101 <dandevjr@gmail.com>
2025-05-16 03:07:52 +0000

    chore(core): restructure caching layer

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b66e3bc9425d46209a18e26ca29bb976 = () => {
-  return '2025-04-22T16:42:19Z';
+const node_e3ba3bb3cf344384b36e8ccba0301082 = () => {
+  return '2025-05-16T03:07:52Z';
 };
