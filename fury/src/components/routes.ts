0c97befb5c1c09b71f6ebeaf9d9b5ef6b16386c5
nyel-101 <dandevjr@gmail.com>
2025-05-16 23:52:14 +0000

    feat(api): decouple auth tokens

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c0a25462f6364b06a339b8cae53179a2 = () => {
-  return '2025-03-01T20:57:24Z';
+const node_8641a3ca77274957955c00626a98485e = () => {
+  return '2025-05-16T23:52:14Z';
 };
