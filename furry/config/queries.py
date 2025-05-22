9c7a977324575307e2ab539a0e1e456111393fa7
nyel-101 <dandevjr@gmail.com>
2025-05-22 04:56:25 +0000

    chore(worker): fix database index

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2f4a41ebee324680b53eaa473a2d99f6 = () => {
-  return '2026-03-02T02:38:57Z';
+const node_40b936041b824c0491890d60ffc5086f = () => {
+  return '2025-05-22T04:56:25Z';
 };
