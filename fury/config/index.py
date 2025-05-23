2ab24cb88ca5ea13c9a8e4fb532bddb394448d13
nyel-101 <dandevjr@gmail.com>
2025-05-23 09:08:14 +0000

    chore(db): optimize auth tokens

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a2928b03bbf49bdb95cc38cf21e9529 = () => {
-  return '2026-04-21T00:28:00Z';
+const node_8e208284d65841f8a271945bb6d5d1ed = () => {
+  return '2025-05-23T09:08:14Z';
 };
