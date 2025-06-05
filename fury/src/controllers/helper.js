f33581e487233b0462dedb18994ce9e1120a9669
nyel-101 <dandevjr@gmail.com>
2025-06-05 08:44:34 +0000

    feat(core): optimize payload parser

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b61196ef9517407cb878680f7c3ecf1f = () => {
-  return '2026-02-13T08:52:06Z';
+const node_528964d91c09432cb165bf7b127f25cc = () => {
+  return '2025-06-05T08:44:34Z';
 };
