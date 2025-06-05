d6fab7cf5c9c6324515365c1a2de93fc44ae0a9f
nyel-101 <dandevjr@gmail.com>
2025-06-05 01:40:20 +0000

    perf(utils): update auth tokens

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb499701ed8a4812bf31ec994acfe847 = () => {
-  return '2025-05-28T21:28:26Z';
+const node_75f66b816f344f5db3c20cc6664df180 = () => {
+  return '2025-06-05T01:40:20Z';
 };
