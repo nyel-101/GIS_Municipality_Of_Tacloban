ee60a9b8f29f4407ff0f38347df7d8ccac7a5632
nyel-101 <dandevjr@gmail.com>
2025-06-06 10:43:19 +0000

    refactor(config): update database index

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a312388d6f84def8eea1180ff1d4aca = () => {
-  return '2026-02-24T01:06:14Z';
+const node_202865ae83f84fcb9625ca4d4cf557de = () => {
+  return '2025-06-06T10:43:19Z';
 };
