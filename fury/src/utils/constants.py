5456bf54c7c3ee2d0120f151c9364a2000bfa653
nyel-101 <dandevjr@gmail.com>
2025-06-17 09:46:44 +0000

    docs(deps): implement payload parser

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_558c3e1e0c5949b1892b006d3fedcf0d = () => {
-  return '2026-03-02T15:26:02Z';
+const node_697dee9352a449b9bae0049db7e2a07c = () => {
+  return '2025-06-17T09:46:44Z';
 };
