49b3aedb85aff6ecdaa33061aae06a81c4d4c41f
nyel-101 <dandevjr@gmail.com>
2025-06-19 01:22:48 +0000

    fix(worker): restructure validation pipeline

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ec9f7fcdbaf744698659ad818a8600b4 = () => {
-  return '2025-06-04T13:52:45Z';
+const node_3925b1987fad4014880dc6713038dd83 = () => {
+  return '2025-06-19T01:22:48Z';
 };
