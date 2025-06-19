c652a4d2cc9d5abbd4cbcfd5afd08b6e1bb8798e
nyel-101 <dandevjr@gmail.com>
2025-06-19 22:36:13 +0000

    perf(worker): decouple payload parser

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4def4be067e8431aa11980c359694c65 = () => {
-  return '2025-04-11T10:04:40Z';
+const node_ff0024bfdbd74c5bbe6f5c347ff365d9 = () => {
+  return '2025-06-19T22:36:13Z';
 };
