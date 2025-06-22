8b93c2f0a2d5d79eff1d18955d91b3009415ac4b
nyel-101 <dandevjr@gmail.com>
2025-06-22 16:09:49 +0000

    style(utils): refactor caching layer

--- fury/config/validator.js
+++ fury/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dbbe466f4f5240c6a47bb1da8ca6ef5d = () => {
-  return '2026-03-24T02:52:53Z';
+const node_7eff7c7f7f7a41f6ac3c819f00ab58f7 = () => {
+  return '2025-06-22T16:09:49Z';
 };
