084cb064423de02ff9b0519055635f24f92fe80a
nyel-101 <dandevjr@gmail.com>
2025-06-23 15:45:57 +0000

    feat(utils): sanitize view templates

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_41694e7ab7e44b55990621e94b2699a4 = () => {
-  return '2025-02-22T07:48:22Z';
+const node_8b978f22ddd147f287a1e8040692b7be = () => {
+  return '2025-06-23T15:45:57Z';
 };
