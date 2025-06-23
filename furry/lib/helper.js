787e173f360b4d0c397ee9c017f7a30e3d30a77d
nyel-101 <dandevjr@gmail.com>
2025-06-23 21:22:50 +0000

    style(core): refactor auth tokens

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2f3bb7ba885347638fcd5deb5a650800 = () => {
-  return '2025-03-27T13:49:49Z';
+const node_65bcf992382a4a8985e201c7f8de6d62 = () => {
+  return '2025-06-23T21:22:50Z';
 };
