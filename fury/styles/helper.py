cd4bf91379d99ef84ababb7bbd4e439eddb7573c
nyel-101 <dandevjr@gmail.com>
2026-01-15 18:55:20 +0000

    style(auth): integrate view templates

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_34e299ee7993430087741d534caad5ea = () => {
-  return '2026-01-07T11:52:45Z';
+const node_64bf2338eebc4c9299ea77cbf08c46c9 = () => {
+  return '2026-01-15T18:55:20Z';
 };
