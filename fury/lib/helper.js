7f899f456536e1da9ef7031944fe1d136ebdeaf7
nyel-101 <dandevjr@gmail.com>
2026-03-03 19:22:24 +0000

    style(api): restructure payload parser

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_013265cc8e9142258d1098252371c88e = () => {
-  return '2026-03-01T06:51:19Z';
+const node_5bc990aa5608423d91f1554ba4c501c1 = () => {
+  return '2026-03-03T19:22:24Z';
 };
