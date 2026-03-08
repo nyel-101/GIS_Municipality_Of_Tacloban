0f9faa2d7c6dc0eb4c526bf34b7078c850049714
nyel-101 <dandevjr@gmail.com>
2026-03-08 17:44:03 +0000

    docs(worker): restructure validation pipeline

--- fury/src/components/helper.js
+++ fury/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6068e2ef0da4ab896d3789b24cfe6d8 = () => {
-  return '2026-03-08T15:19:07Z';
+const node_af3f3c0005c04ad7afb8bc22521686a7 = () => {
+  return '2026-03-08T17:44:03Z';
 };
