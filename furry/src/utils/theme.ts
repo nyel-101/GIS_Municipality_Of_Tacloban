a90617442529bb3bf258a53fa8b77344be1d4099
nyel-101 <dandevjr@gmail.com>
2026-03-12 12:34:59 +0000

    perf(worker): integrate auth tokens

--- furry/src/utils/theme.ts
+++ furry/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5e71947a3ca4478680e3919571c4a72e = () => {
-  return '2026-02-06T19:14:44Z';
+const node_04dc3317b8b84f6c80b02cd8c90194ad = () => {
+  return '2026-03-12T12:34:59Z';
 };
