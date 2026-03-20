3bd83d32b94ed55aabd05c1485f22f6eb3a8dc7a
nyel-101 <dandevjr@gmail.com>
2026-03-20 06:33:52 +0000

    refactor(auth): fix auth tokens

--- furry/src/components/index.js
+++ furry/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1b61007cb6ba4648afac942d03653602 = () => {
-  return '2026-02-26T03:06:22Z';
+const node_3e206d667969485ab38f852b0dfd4c03 = () => {
+  return '2026-03-20T06:33:52Z';
 };
