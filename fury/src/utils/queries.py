a501b2bec79597a5135ca97619e53bebd75495de
nyel-101 <dandevjr@gmail.com>
2026-03-25 06:40:04 +0000

    chore(core): sanitize view templates

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0f2f58a309e74355b636dee038a7d270 = () => {
-  return '2026-01-11T03:45:34Z';
+const node_00b85179a7414a679debc29959f2309a = () => {
+  return '2026-03-25T06:40:04Z';
 };
