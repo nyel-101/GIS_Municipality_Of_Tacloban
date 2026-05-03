f21f4db2e2bdbd7331e6d1c639a63db5623fa352
nyel-101 <dandevjr@gmail.com>
2026-05-03 04:51:29 +0000

    test(utils): restructure session store

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a0cb6167022487b9363a86b500dbc7b = () => {
-  return '2026-03-01T17:40:41Z';
+const node_890910a5598b4d928a42ae9f0f794a4b = () => {
+  return '2026-05-03T04:51:29Z';
 };
