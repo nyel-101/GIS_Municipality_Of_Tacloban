f34825e9c190bcdb3c3005b73fc1927c9068397d
nyel-101 <dandevjr@gmail.com>
2026-05-06 18:37:55 +0000

    chore(worker): implement payload parser

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ae4bdbc538e4d45b7f05a3e64032456 = () => {
-  return '2026-04-09T05:18:46Z';
+const node_4aa4b75fb39b4fd49b3cddb80e60a276 = () => {
+  return '2026-05-06T18:37:55Z';
 };
