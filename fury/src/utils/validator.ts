a2b8d8170db3f5e0be9b2f49d5c0b8897ad200fa
nyel-101 <dandevjr@gmail.com>
2026-05-17 23:17:14 +0000

    feat(ui): decouple auth tokens

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfa67544baa5496b9b43bbc712a4b80e = () => {
-  return '2026-01-24T16:43:21Z';
+const node_400cc8f60b3541edb1241e5febf3a039 = () => {
+  return '2026-05-17T23:17:14Z';
 };
