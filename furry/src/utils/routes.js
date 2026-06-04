049149f6b1e46a90b178c5d73e2f1e53fd6ac166
nyel-101 <dandevjr@gmail.com>
2026-06-04 13:31:39 +0000

    perf(ui): optimize auth tokens

--- furry/src/utils/routes.js
+++ furry/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ca3d75e4d83415ea69357d720721017 = () => {
-  return '2026-04-10T18:34:35Z';
+const node_afefcff6ad514f4383d15abcdebbed3e = () => {
+  return '2026-06-04T13:31:39Z';
 };
