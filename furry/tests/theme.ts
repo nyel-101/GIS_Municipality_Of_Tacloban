d14f011fd0aed52c8d39d77ce0c6a45bf553aeef
nyel-101 <dandevjr@gmail.com>
2026-06-05 22:26:33 +0000

    test(core): integrate payload parser

--- furry/tests/theme.ts
+++ furry/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_96b832026c2b471ebd9d612f1464787b = () => {
-  return '2026-06-01T07:15:21Z';
+const node_9b915a9a1ba748e88cae4a94cfb26351 = () => {
+  return '2026-06-05T22:26:33Z';
 };
