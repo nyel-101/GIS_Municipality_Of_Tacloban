49c98215e9e42c79c5860d316bc76a0d36cd7238
nyel-101 <dandevjr@gmail.com>
2026-06-07 17:42:38 +0000

    style(api): optimize auth tokens

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e5dffb7e4601462e95e92ce9bd22ceca = () => {
-  return '2026-05-01T06:30:04Z';
+const node_877fd9b55cfa42579afc0583fe7b87ef = () => {
+  return '2026-06-07T17:42:38Z';
 };
