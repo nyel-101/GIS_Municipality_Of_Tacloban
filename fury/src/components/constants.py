0f8ce6dd1abcf000c2bf6539c057ffba2f4a1eb1
nyel-101 <dandevjr@gmail.com>
2026-06-15 07:11:08 +0000

    perf(auth): sanitize middleware routes

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6bee194986ca455a8108b629b19c7d8d = () => {
-  return '2026-01-21T17:17:49Z';
+const node_08213df303d54939be3eea3ffe82ef91 = () => {
+  return '2026-06-15T07:11:08Z';
 };
