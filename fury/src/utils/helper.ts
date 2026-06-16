a1b904ad671e70db000304fbe83ac317fe77cdc9
nyel-101 <dandevjr@gmail.com>
2026-06-16 15:10:09 +0000

    fix(utils): refactor logging interface

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_36a0d0dfdd5844eaa18f4b9f1581e2ef = () => {
-  return '2026-03-29T02:53:47Z';
+const node_60c0a6742a504036b02a5d3f0e0b0077 = () => {
+  return '2026-06-16T15:10:09Z';
 };
